c0a2d9a799e0aea175b5ba4067e26ed2ccf26a2b
Ruben Schlaen <gschlaen@agro.uba.ar>
2022-01-03 17:36:43 -0300

    Show OnMessage Notification

--- lib/services/push_notifications_service.dart
+++ lib/services/push_notifications_service.dart
@@ -3,6 +3,7 @@
 import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -12,6 +13,15 @@ class PushNotificationService {
   static Stream<String> get mesaggeStream => _messageStream.stream;
   // getter para acceder al stream por fuera de la clase
 
+  static const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description: 'This channel is used for important notifications.', // description
+    importance: Importance.max,
+  );
+
+  static final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
   static Future _backgroundHandler(RemoteMessage message) async {
     // print('background Handler ${message.messageId}');
     _messageStream.add(message.data['product'] ?? 'No Data');
@@ -19,6 +29,31 @@ class PushNotificationService {
 
   static Future _onMessageHandler(RemoteMessage message) async {
     // print('onMessage Handler ${message.messageId}');
+
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    RemoteNotification? notification = message.notification;
+    String iconName = const AndroidInitializationSettings('@mipmap/ic_launcher').defaultIcon.toString();
+
+    // Si `onMessage` es activado con una notificación, construimos nuestra propia
+    // notificación local para mostrar a los usuarios, usando el canal creado.
+    if (notification != null) {
+      flutterLocalNotificationsPlugin.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              channel.id,
+              channel.name,
+              channelDescription: channel.description,
+              icon: iconName,
+            ),
+          ));
+    }
+
     _messageStream.add(message.data['product'] ?? 'No Data');
   }
 
